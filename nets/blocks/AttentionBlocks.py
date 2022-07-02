08e303c57842a1963abacf70bae2a79733e2132b
paperchae <forownsake@gmail.com>
2022-07-02 19:10:35 +0900

    appreanceadded attention block to appearance model

--- nets/blocks/AttentionBlocks.py
+++ nets/blocks/AttentionBlocks.py
@@ -1,17 +1,18 @@
 import torch
 from torch.nn import Module
+from torch import linalg as LA
 
 
 class AttentionBlock(Module):
     def __init__(self, in_channels):
+        # 1x1 Convolution filter
         self.attention = torch.nn.Conv2d(in_channels=in_channels, out_channels=1,
-                                         kernel_size=(1, 1), stride=(1, 1))
+                                         kernel_size=1, stride=1, padding=1)
 
-    def forward(self, input):
-        at1 = self.attention(input)
-        at2 = torch.sigmoid(at1)
-        l1norm = torch.norm(at2, p=1)
-        batchN, channelOut, heightOut, widthOut = input.shape
-        at3 = torch.div(heightOut * widthOut * at2, 2 * l1norm)
+    def forward(self, inputs):
+        at = torch.sigmoid(self.attention(inputs))
+        l1norm = LA.norm(at, 1)
+        batchN, channelOut, heightOut, widthOut = inputs.shape
+        at = torch.div(heightOut * widthOut * at, 2 * l1norm)
 
-        return at3
+        return at
--- nets/models/sub_models/AppearanceModel.py
+++ nets/models/sub_models/AppearanceModel.py
@@ -1,20 +1,20 @@
-from nets.blocks import attentionBlocks
 import torch
 from torch.nn import Module
+from nets.blocks.AttentionBlocks import AttentionBlock
 
-
-class ApperanceModel_2D(Module):
+# input :
+class AppearanceModel_2D(Module):
     # in_channels = 3 , out_channels = 32, conv_kernel_size = (3,3)
     def __init__(self, in_channels, out_channels, kernel_size):
         super().__init__()
         # Convolution 3x3 kernel Layer 1 (3@36x36 -> 32@36x36)
         self.a_conv1 = torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                                       kernel_size=kernel_size)
+                                       kernel_size=kernel_size, stride=1, padding=1)
         self.a_batch_Norm1 = torch.nn.BatchNorm2d(out_channels)
 
         # Convolution 3x3 kernel Layer 2 (32@36x36 -> 32@36x36)
         self.a_conv2 = torch.nn.Conv2d(in_channels=out_channels, out_channels=out_channels,
-                                       kernel_size=kernel_size)
+                                       kernel_size=kernel_size, stride=1, padding=1)
         self.a_batch_Norm2 = torch.nn.BatchNorm2d(out_channels)
 
         # Average-pooling 2x2 kernel Layer 3 (32@36x36 -> 32@18x18)
@@ -22,18 +22,18 @@ class ApperanceModel_2D(Module):
 
         # Convolution 3x3 kernel Layer 4 (32@18x18 -> 64@18x18)
         self.a_conv3 = torch.nn.Conv2d(in_channels=out_channels, out_channels=out_channels * 2,
-                                       kernel_size=kernel_size)
+                                       kernel_size=kernel_size, stride=1, padding=1)
         self.a_batch_Norm3 = torch.nn.BatchNorm2d(out_channels * 2)
 
         # Convolution 3x3 kernel Layer 5 (64@18x18 -> 64@18x18)
         self.a_conv4 = torch.nn.Conv2d(in_channels=out_channels * 2, out_channels=out_channels * 2,
-                                       kernel_size=kernel_size)
+                                       kernel_size=kernel_size, stride=1, padding=1)
         self.a_batch_Norm4 = torch.nn.BatchNorm2d(out_channels * 2)
 
         # Average-Pooling 2x2 kernel layer 6 (64@18x18 -> 64@9x9)
         self.avg_pool2 = torch.nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-    # Apperance Model forward
+    # Appearance Model forward
     def forward(self, inputs):
         # Convolution 3x3 kernel Layer 1 (3@36x36 -> 32@36x36)
         ap1 = torch.tanh(self.a_batch_Norm1(self.a_conv1(inputs)))
@@ -42,12 +42,12 @@ class ApperanceModel_2D(Module):
         # Average-pooling 2x2 kernel Layer 3 (32@36x36 -> 32@18x18)
         ap3 = self.avg_pool1(ap2)
         # Attention 1
-        # at1 = (ap2)
+        at1 = AttentionBlock(ap2)
         # Convolution 3x3 kernel Layer 4 (32@18x18 -> 64@18x18)
         ap4 = torch.tanh(self.a_batch_Norm3(self.a_conv3(ap3)))
         # Convolution 3x3 kernel Layer 5 (64@18x18 -> 64@18x18)
         ap5 = torch.tanh(self.a_batch_Norm4(self.a_conv4(ap4)))
-        # Average-Pooling 2x2 kernel layer 6 (64@18x18 -> 64@9x9)
-        ap6 = self.avg_pool2(ap5)
         # Attention 2
-        # at2 = (ap5)
+        at2 = AttentionBlock(ap5)
+
+        return at1, at2
